e61d27a5c69f47223111971bddc031b454e2bb3c
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-03-01 16:40:56 +0100

    Initial implementation.

---
+++ index.ts
@@ -0,0 +1,80 @@
+import { spawn, SpawnOptions } from "child_process";
+import type { Plugin as VitePlugin } from "vite";
+
+// Utility to invoke a given sbt task and fetch its output
+function printSbtTask(task: string, cwd?: string): Promise<string> {
+  const args = ["--batch", "-no-colors", "-Dsbt.supershell=false", `print ${task}`];
+  const options: SpawnOptions = {
+    cwd: cwd,
+    stdio: ['ignore', 'pipe', 'inherit'],
+  };
+  const child = process.platform === 'win32'
+    ? spawn("sbt.bat", args.map(x => `"${x}"`), {shell: true, ...options})
+    : spawn("sbt", args, options);
+
+  let fullOutput: string = '';
+
+  child.stdout!.setEncoding('utf-8');
+  child.stdout!.on('data', data => {
+    fullOutput += data;
+    process.stdout.write(data); // tee on my own stdout
+  });
+
+  return new Promise((resolve, reject) => {
+    child.on('error', err => {
+      reject(new Error(`sbt invocation for Scala.js compilation could not start. Is it installed?\n${err}`));
+    });
+    child.on('close', code => {
+      if (code !== 0)
+        reject(new Error(`sbt invocation for Scala.js compilation failed with exit code ${code}.`));
+      else
+        resolve(fullOutput.trimEnd().split('\n').at(-1)!);
+    });
+  });
+}
+
+export interface ScalaJSPluginOptions {
+  cwd?: string,
+  projectID?: string,
+  uriPrefix?: string,
+}
+
+export default function scalaJSPlugin(options: ScalaJSPluginOptions = {}): VitePlugin {
+  const { cwd, projectID, uriPrefix } = options;
+
+  const fullURIPrefix = uriPrefix ? (uriPrefix + ':') : 'scalajs:';
+
+  let isDev: boolean | undefined = undefined;
+  let scalaJSOutputDir: string | undefined = undefined;
+
+  return {
+    name: "scalajs:sbt-scalajs-plugin",
+
+    // Vite-specific
+    configResolved(resolvedConfig) {
+      isDev = resolvedConfig.mode === 'development';
+    },
+
+    // standard Rollup
+    async buildStart(options) {
+      if (isDev === undefined)
+        throw new Error("configResolved must be called before buildStart");
+
+      const task = isDev ? "fastLinkJSOutput" : "fullLinkJSOutput";
+      const projectTask = projectID ? `${projectID}/${task}` : task;
+      scalaJSOutputDir = await printSbtTask(projectTask, cwd);
+    },
+
+    // standard Rollup
+    resolveId(source, importer, options) {
+      if (scalaJSOutputDir === undefined)
+        throw new Error("buildStart must be called before resolveId");
+
+      if (!source.startsWith(fullURIPrefix))
+        return null;
+      const path = source.substring(fullURIPrefix.length);
+
+      return `${scalaJSOutputDir}/${path}`;
+    },
+  };
+}
---
+++ test/plugin.test.ts
@@ -0,0 +1,115 @@
+import { beforeEach, describe, expect, it, TestOptions } from 'vitest';
+import scalaJSPlugin, { ScalaJSPluginOptions } from "../index";
+import type { PluginContext } from 'rollup';
+import type { Plugin as VitePlugin } from "vite";
+
+/* This interface refines the VitePlugin with some knowledge about our
+ * particular implementation, for easier testing. If we define here something
+ * that is incompatible with what's in VitePlugin, the compiler complains, so
+ * we do get some safety that we adhere to VitePlugin's API.
+ */
+interface RefinedPlugin extends VitePlugin {
+  configResolved: (this: void, resolvedConfig: { mode: string }) => void | Promise<void>;
+  buildStart: (this: PluginContext, options: {}) => void | Promise<void>;
+  resolveId: (this: PluginContext, source: string) => string | Promise<string>;
+}
+
+function normalizeSlashes(path: string | null): string | null {
+  return path === null ? null : path.replace(/\\/g, '/');
+}
+
+const MODE_DEVELOPMENT = 'development';
+const MODE_PRODUCTION = 'production';
+
+describe("scalaJSPlugin", () => {
+  const cwd = process.cwd() + "/test/testproject";
+
+  const testOptions: TestOptions = {
+    timeout: 60000, // running sbt takes time
+  };
+
+  const setup: (options: ScalaJSPluginOptions) => [RefinedPlugin, PluginContext] = (options) => {
+    const plugin = scalaJSPlugin({ cwd: cwd, ...options }) as RefinedPlugin;
+    const fakePluginContext = {} as PluginContext;
+    return [plugin, fakePluginContext];
+  }
+
+  /* Wait for 2 seconds between tests to let sbt close its server.
+   * Without this, we get spurious failures with
+   * > sbt thinks that server is already booting because of this exception:
+   * > sbt.internal.ServerAlreadyBootingException: java.io.IOException:
+   *   Could not create lock for \\.\pipe\sbt-load3101661995253037154_lock, error 5
+   */
+  beforeEach(() => {
+    return new Promise(resolve => {
+      setTimeout(resolve, 2000);
+    });
+  });
+
+  it("works without a specific projectID (production)", async () => {
+    const [plugin, fakePluginContext] = setup({});
+
+    await plugin.configResolved.call(undefined, { mode: MODE_PRODUCTION });
+    await plugin.buildStart.call(fakePluginContext, {});
+
+    expect(normalizeSlashes(await plugin.resolveId.call(fakePluginContext, 'scalajs:main.js')))
+      .toContain('/testproject/target/scala-3.2.2/testproject-opt/main.js');
+
+    expect(await plugin.resolveId.call(fakePluginContext, 'scalajs/main.js'))
+      .toBeNull();
+  }, testOptions);
+
+  it("works without a specific projectID (development)", async () => {
+    const [plugin, fakePluginContext] = setup({});
+
+    await plugin.configResolved.call(undefined, { mode: MODE_DEVELOPMENT });
+    await plugin.buildStart.call(fakePluginContext, {});
+
+    expect(normalizeSlashes(await plugin.resolveId.call(fakePluginContext, 'scalajs:main.js')))
+      .toContain('/testproject/target/scala-3.2.2/testproject-fastopt/main.js');
+
+    expect(await plugin.resolveId.call(fakePluginContext, 'scalajs/main.js'))
+      .toBeNull();
+  }, testOptions);
+
+  it("works with a specific projectID (production)", async () => {
+    const [plugin, fakePluginContext] = setup({
+      projectID: "otherProject",
+    });
+
+    await plugin.configResolved.call(undefined, { mode: MODE_PRODUCTION });
+    await plugin.buildStart.call(fakePluginContext, {});
+
+    expect(normalizeSlashes(await plugin.resolveId.call(fakePluginContext, 'scalajs:main.js')))
+      .toContain('/testproject/other-project/target/scala-3.2.2/otherproject-opt/main.js');
+
+    expect(await plugin.resolveId.call(fakePluginContext, 'scalajs/main.js'))
+      .toBeNull();
+  }, testOptions);
+
+  it("works with a custom URI prefix (development)", async () => {
+    const [plugin, fakePluginContext] = setup({
+      uriPrefix: "customsjs",
+    });
+
+    await plugin.configResolved.call(undefined, { mode: MODE_DEVELOPMENT });
+    await plugin.buildStart.call(fakePluginContext, {});
+
+    expect(normalizeSlashes(await plugin.resolveId.call(fakePluginContext, 'customsjs:main.js')))
+      .toContain('/testproject/target/scala-3.2.2/testproject-fastopt/main.js');
+
+    expect(await plugin.resolveId.call(fakePluginContext, 'scalajs:main.js'))
+      .toBeNull();
+  }, testOptions);
+
+  it("does not work with a project that does not link", async () => {
+    const [plugin, fakePluginContext] = setup({
+      projectID: "invalidProject",
+    });
+
+    await plugin.configResolved.call(undefined, { mode: MODE_PRODUCTION });
+
+    const buildStartResult = plugin.buildStart.call(fakePluginContext, {});
+    expect(buildStartResult).rejects.toContain('sbt invocation');
+  }, testOptions);
+});
